c056d1631f74b57840ff57057d305ec0a1380ce1
Anand-Velpuri <129038384+Anand-Velpuri@users.noreply.github.com>
2025-01-18 15:45:51 +0530

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+const About = () => {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <h1 className="text-6xl mb-4 font-bold">Github Finder</h1>
+            <p className="text-lg text-gray-700 mb-4">
+                Github Finder is a simple and powerful tool that helps developers search for GitHub profiles and view detailed information, including repositories, followers, and more. It's designed to make navigating GitHub's vast developer ecosystem effortless and efficient.
+            </p>
+            <p className="text-md text-gray-600">
+                Version: <span className="font-medium">1.0.0</span>
+            </p>
+            <p className="text-md text-gray-600">
+                Developed by: <span className="font-medium">Anand Velpuri</span>
+            </p>
+        </div>
+    );
+};
+
+export default About;
